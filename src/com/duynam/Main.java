bca88df811954e065a33d7c0e8c354615462e97c
doduynam <39905671+doduynam@users.noreply.github.com>
2022-03-19 22:58:25 +0700

    Update some buffer

--- src/com/duynam/Main.java
+++ src/com/duynam/Main.java
@@ -1,8 +1,15 @@
 package com.duynam;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<SlangWord> slangWordList = ReadWriteFile.readFile("slang.txt");
+        for(SlangWord sl : slangWordList) {
+            System.out.println(sl);
+        }
+
+        ReadWriteFile.writeFile(slangWordList, "slang1.txt");
     }
 }
